812ae15cbf6b431bc4413dfba5558ca8f808e5fa
José Lisboa Filho <jlisboa600@gmail.com>
2025-02-23 21:10:47 -0300

    SRP WRITE Atualização

--- SOLID_MPP_ATVS/SOLID_MPP_ATVS/SOLID_SRP_A.cs
+++ SOLID_MPP_ATVS/SOLID_MPP_ATVS/SOLID_SRP_A.cs
@@ -1,17 +1,27 @@
 class SOLID_SRP_ATV
-{ 
-//
-static void Main(string[] args) { } 
-//
-static void CreateClient(){ } 
-//
-static void ReadClient() { } 
-//
-static void UpdateClient(){ } 
-//
-static void DeleteClient() { } 
-//
-static void NotifyClient() { 
-// Enviar e-mail / SMS 
-} 
+{
+    //
+    static void Main(string[] args) { }
+    //
+    static void CreateClient() {
+        Console.WriteLine("Cliente Criado");
+    }
+    //
+    static void ReadClient() {
+        Console.WriteLine("Cliente Atualizado");
+    }
+    //
+    static void UpdateClient() {
+        Console.WriteLine("Cliente Atualizado");
+    }
+    //
+    static void DeleteClient() {
+        Console.WriteLine("Cliente Deletado!");
+    }
+    //
+    static void NotifyClient()
+    {
+        // Enviar e-mail / SMS
+        Console.WriteLine("SMS e/ou Email Enviado(s)!");
+    }
 }
--- SOLID_MPP_ATVS/SOLID_MPP_ATVS/obj/Debug/net9.0/SOLID_MPP_ATVS.AssemblyInfo.cs
+++ SOLID_MPP_ATVS/SOLID_MPP_ATVS/obj/Debug/net9.0/SOLID_MPP_ATVS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOLID_MPP_ATVS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3045f56f7c1981b12a2a79f76cfccc26bd9d1842")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7516203d510563a5fd2f23deda49c8382a91b05a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOLID_MPP_ATVS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOLID_MPP_ATVS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
