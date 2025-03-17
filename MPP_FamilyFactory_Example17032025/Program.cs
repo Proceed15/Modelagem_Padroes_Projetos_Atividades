199c10738772f968a104eb5d50091e913d4ddff8
JOSE ROBERTO LISBOA DA SILVA FILHO <0031432412016@lti.fatecso.edu.br>
2025-03-17 14:29:21 -0300

    AbstractFactoryExample

---
+++ MPP_FamilyFactory_Example17032025/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// Abstract Product
+public interface IBotao {
+    void Render();
+}
+// Concrete Products
+public class BotaoWindows : IBotao {
+    public void Render() => Console.WriteLine("Renderizando um botão no Windows");
+}
+
+public class BotaoMac : IBotao {
+    public void Render() => Console.WriteLine("Renderizando um botão no macOS");
+}
+// Abstract Factories 
+public interface IFactory {
+    IBotao CriarBotao();
+}
+// Concrete Factories
+public class Windowsfactory : IFactory {
+    public IBotao CriarBotao() => new BotaoWindows();
+}
+public class MacFactory : IFactory {
+    public IBotao CriarBotao() => new BotaoMac();
+}
+//Cliente
+class Program {
+    static void Main() {
+        IFactory factory = new Windowsfactory(); //Pode-se trocar para MacFactory
+        IBotao botao = factory.CriarBotao();
+        botao.Render();
+    }
+}
