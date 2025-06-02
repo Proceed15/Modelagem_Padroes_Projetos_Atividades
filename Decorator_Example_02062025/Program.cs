223e4294c80dac2a657bf00fe55f9e056f4961aa
JOSE ROBERTO LISBOA DA SILVA FILHO <0031432412016@lti.fatecso.edu.br>
2025-06-02 15:40:21 -0300

    Atividade Notificação Decorator Whatsapp

--- Decorator_Example_02062025/Program.cs
+++ Decorator_Example_02062025/Program.cs
@@ -12,6 +12,7 @@ namespace DecoratorPatternExample
         {
             // Notificação simples por e-mail
             INotifier emailNotifier = new EmailNotifier();
+            INotifier whatsappNotifier = new WhatsappNotifier();
             Console.WriteLine("--- Notificação Simples por Email ---");
             Console.WriteLine(emailNotifier.Send("Olá, Mundo!"));
             Console.WriteLine();
@@ -41,8 +42,13 @@ namespace DecoratorPatternExample
             INotifier fullNotifierNested = new SlackNotifierDecorator(
                                                new SMSNotifierDecorator(
                                                    new EmailNotifier()
-                                               )
+                                               )                                           
                                            );
+            INotifier WhatsappNotifierNested = new SlackNotifierDecorator(
+                                                new WhatsappNotifierDecorator(
+                                                    new WhatsappNotifier()
+                                               )
+                                            );
             Console.WriteLine("--- (Alternativo) Notificação por Email + SMS + Slack ---");
             Console.WriteLine(fullNotifierNested.Send("Lançamento da Nova Funcionalidade Aninhado!"));
             Console.WriteLine();
@@ -51,8 +57,21 @@ namespace DecoratorPatternExample
             // Apenas E-mail + Slack (sem SMS no meio)
             INotifier emailNotifierBaseForSlack = new EmailNotifier();
             INotifier emailAndSlackNotifier = new SlackNotifierDecorator(emailNotifierBaseForSlack);
-            Console.WriteLine("--- Notificação por Email + Slack ---");
+            Console.WriteLine("--- Notificação por Email + Whatsapp ---");
             Console.WriteLine(emailAndSlackNotifier.Send("Reunião de Equipe Amanhã."));
+            Console.WriteLine();
+            //Atividade
+            Console.WriteLine("--- (Atividade) Notificação por Whatsapp---");
+            //Atividade com Whatsapp e Slack
+            Console.WriteLine(WhatsappNotifierNested.Send("Lançamento da Nova Funcionalidade Aninhada!"));
+            Console.WriteLine();
+
+
+            // Apenas Whatsapp (sem SMS no meio)
+            INotifier Whatsapp = new WhatsappNotifier();
+            INotifier WhatsappDouble = new WhatsappNotifierDecorator(Whatsapp);
+            Console.WriteLine("--- Notificação por Whatsapp ---");
+            Console.WriteLine(WhatsappDouble.Send("Reunião de Equipe Amanhã."));
             Console.WriteLine();
         }
     }
