94e4b7744b3bbf59f056a1d7feac1e5655ef39bf
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2025-09-08 17:55:04 -0300

    Update CloseDoorCommand.cs

--- MPP_Command_Activity_08092025/Command/CloseDoorCommand.cs
+++ MPP_Command_Activity_08092025/Command/CloseDoorCommand.cs
@@ -3,4 +3,5 @@ public class CloseDoorCommand : ICommand
     private Door Door;
     public CloseDoorCommand(Door p) => Door = p;
     public bool Run() => Door.CloseDoor();
+    public bool Desfazer() => Door.open();
 }
