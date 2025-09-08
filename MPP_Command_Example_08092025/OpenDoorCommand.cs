aea1c3360b3bc313717e3016564848e330c958a9
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2025-09-08 18:11:07 -0300

    Atividade

--- MPP_Command_Example_08092025/OpenDoorCommand.cs
+++ MPP_Command_Example_08092025/OpenDoorCommand.cs
@@ -3,4 +3,5 @@ public class OpenDoorCommand : ICommand
     private Door Door;
     public OpenDoorCommand(Door p) => Door = p;
     public bool Run() => Door.open();
+    public bool Desfazer() => Door.CloseDoor();
 }
