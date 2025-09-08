ebb1ff8fbb59244466ffd87b56c8a4aa8c3f3276
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2025-09-08 18:13:06 -0300

    Update DesTurnONLightCommand.cs

--- MPP_Command_Example_08092025/DesTurnONLightCommand.cs
+++ MPP_Command_Example_08092025/DesTurnONLightCommand.cs
@@ -2,5 +2,14 @@ public class DesTurnONLightCommand : ICommand
 {
     private Light Light;
     public DesTurnONLightCommand(Light l) => Light = l;
-    public bool Run() { Light.DesTurnON(); return true; }
+    public bool Run()
+    {
+        Light.DesTurnON();
+        return true;
+    }
+    public bool Desfazer()
+    {
+        Light.TurnON();
+        return true;
+    }
 }
