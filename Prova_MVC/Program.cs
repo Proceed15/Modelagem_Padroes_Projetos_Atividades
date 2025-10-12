f98d53085c9ca76b23e88f169967ac2291f5da99
JRLSF <jlisboa600@gmail.com>
2025-10-11 23:08:32 -0300

    Prova V1 Estudo Programação

---
+++ Prova_MVC/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Agenda model = new Agenda();
+        //ContatoView view = new ContatoView();
+        //AgendaController controller = new AgendaController(model, view);
+        //controller.iniciar();
+    }
+}
