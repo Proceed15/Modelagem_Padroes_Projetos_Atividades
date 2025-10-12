25707fa18fa74012671f18143c541437b17c7b40
JRLSF <jlisboa600@gmail.com>
2025-10-11 22:32:55 -0300

    Overall Files Project Update

--- MPP_Example_State_29092025/obj/Debug/net9.0/MPP_Example_State_29092025.AssemblyInfo.cs
+++ MPP_Example_State_29092025/obj/Debug/net9.0/MPP_Example_State_29092025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MPP_Example_State_29092025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004532069c510d2e032d7661341b1967b5152897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da926c3cc92a7b887481c6e7fb212d23378431e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MPP_Example_State_29092025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MPP_Example_State_29092025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProvaAtv10Respostas/obj/Debug/net9.0/ProvaAtv10Respostas.AssemblyInfo.cs
+++ ProvaAtv10Respostas/obj/Debug/net9.0/ProvaAtv10Respostas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProvaAtv10Respostas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004532069c510d2e032d7661341b1967b5152897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da926c3cc92a7b887481c6e7fb212d23378431e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProvaAtv10Respostas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProvaAtv10Respostas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
